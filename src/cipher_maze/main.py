ba15fb25e81814e3c944d8eef4f9586d3ad8513b
Adi <adisneh@gmail.com>
2021-09-07 15:20:31 +0300

    Map point diffs to directions, set default point in _generate_maze

--- src/cipher_maze/main.py
+++ src/cipher_maze/main.py
@@ -1,6 +1,5 @@
 from cipher_maze.game import play
 from cipher_maze.maze import generate_maze, Point
-from cipher_maze.ui import print_maze
 
 MAZE_WIDTH = 5
 MAZE_HEIGHT = 5
@@ -10,6 +9,5 @@ maze = generate_maze(
     start=Point(x=0, y=0),
     end=Point(x=MAZE_WIDTH - 1, y=MAZE_HEIGHT - 1)
 )
-# print_maze(maze)
 
 play(maze)
